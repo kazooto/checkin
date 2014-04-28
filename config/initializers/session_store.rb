94e28378cb6edcca35d6ef7a5784a9e9f39eecd9
kazooto <k.kazooto@gmail.com>
2014-04-29 00:03:41 +0900

    add facebook notification by omniauth

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-SeatIn::Application.config.session_store :cookie_store, key: '_seat_in_session'
+#SeatIn::Application.config.session_store :cookie_store, key: '_seat_in_session'
+SeatIn::Application.config.session_store :active_record_store
