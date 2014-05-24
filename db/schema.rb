1c9d55e157b6ee7305d50ee42bcc41e11ea18e6c
kazooto <k.kazooto@gmail.com>
2014-05-25 02:45:55 +0900

    名前登録可能に.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140428142412) do
+ActiveRecord::Schema.define(version: 20140524171602) do
 
   create_table "sessions", force: true do |t|
     t.string   "session_id", null: false
@@ -43,6 +43,5 @@ ActiveRecord::Schema.define(version: 20140428142412) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
 
 end
